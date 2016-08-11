5bd836ea8174f323c9821f01e31f3678507873c0
Ascendings <brotherballantine@gmail.com>
2016-08-11 11:46:54 -0400

    Added configuration to the project with the github.com/go-ini/ini module

--- gowis.go
+++ gowis.go
@@ -3,15 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
+
+	"gopkg.in/ini.v1"
 
 	"gogs.ballantine.tech/gballan1/gowis/app"
 )
 
 func main() {
 	// initialize router
-	m := app.InitRouter()
+	mac := app.InitRouter()
+
+	// load the config file
+	cfg, err := ini.InsensitiveLoad("./app/app.ini")
+
+	// check for errors while loading the configuration
+	if err != nil {
+
+	}
 
 	// let the user know we're running!
 	log.Println("Server is running...")
-	log.Println(http.ListenAndServe("0.0.0.0:4000", m))
+	log.Println(http.ListenAndServe(strings.Join([]string{
+		cfg.Section("server").Key("address").String(),
+		cfg.Section("server").Key("port").String()}, ":"), mac))
 }
