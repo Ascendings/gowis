306e22b6c56f2d830f040e084fb5aa0fe808d3ca
Gregory Ballantine <brotherballantine@gmail.com>
2016-08-14 10:11:14 -0400

    Changed the default error handling for form validation

--- app/forms/page.go
+++ app/forms/page.go
@@ -2,7 +2,7 @@ package forms
 
 // CreatePageForm - form used for creating a page
 type CreatePageForm struct {
-	URLSlug       string `form:"url_slug" binding:"required"`
+	URLSlug       string `form:"url_slug" binding:"Required;MinSize(2)"`
 	PageContent   string `form:"page_content"`
 	CommitMessage string `form:"commit_message"`
 }
--- app/router.go
+++ app/router.go
@@ -11,13 +11,15 @@ import (
 
 // InitRouter - initializes the router and sets routes
 func InitRouter(m macaron.Macaron) {
+	bindIgnErr := binding.BindIgnErr
+
 	// create new Wiki controller
 	w := new(controllers.WikiController)
 
 	// define routes
 	m.Get("/", w.Home).Name("wiki.home")
 	m.Get("/list", w.List).Name("wiki.list")
-	m.Combo("/create").Get(w.Create).Post(binding.Bind(forms.CreatePageForm{}), w.PostCreate).Name("wiki.create")
+	m.Combo("/create").Get(w.Create).Post(bindIgnErr(forms.CreatePageForm{}), w.PostCreate).Name("wiki.create")
 	m.Get("/view/:urlSlug", w.View).Name("wiki.view")
-	m.Combo("/edit/:urlSlug").Get(w.Edit).Post(binding.Bind(forms.CreatePageForm{}), w.PostEdit).Name("wiki.edit")
+	m.Combo("/edit/:urlSlug").Get(w.Edit).Post(bindIgnErr(forms.CreatePageForm{}), w.PostEdit).Name("wiki.edit")
 }
