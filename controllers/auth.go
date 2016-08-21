e2b5a3f4316c9440b51d397988645bcbd818e2c7
Gregory Ballantine <brotherballantine@gmail.com>
2016-08-21 00:05:13 -0400

    Added login post route

--- controllers/auth.go
+++ controllers/auth.go
@@ -1,9 +1,11 @@
 package controllers
 
 import (
+  "github.com/astaxie/beego/orm"
   "github.com/go-macaron/session"
 
-  "gogs.ballantine.tech/gballan1/gowis/modules/wiki"
+  "gogs.ballantine.tech/gballan1/gowis/models"
+  "gogs.ballantine.tech/gballan1/gowis/modules/auth"
 
   "gopkg.in/macaron.v1"
 )
@@ -22,6 +24,29 @@ func (a *AuthController) Login(ctx *macaron.Context) {
 }
 
 // PostLogin - login backend
-func (a *AuthController) PostLogin(ctx *macaron.Context, input wiki.PageForm, f *session.Flash) {
+func (a *AuthController) PostLogin(ctx *macaron.Context, input auth.LoginForm, f *session.Flash) {
+  // User model with the email set
+  user := models.User{Email: ctx.Params("identifier")}
 
+  // find the user by email
+  emailErr := models.DB.Read(&user, "email")
+  // check for errors
+  if emailErr == orm.ErrNoRows || emailErr == orm.ErrMissPK {
+    // user model with the username set
+    user = models.User{Username: ctx.Params("identifier")}
+
+    // find the user by username
+    usernameErr := models.DB.Read(&user, "username")
+    // check for errors
+    if usernameErr == orm.ErrNoRows || usernameErr == orm.ErrMissPK {
+      f.Error("Invalid credentials", false)
+      // redirect the user
+      ctx.Redirect(ctx.URLFor("auth.login"))
+    }
+  } else {
+    // set the title
+    f.Success("Successful login", false)
+    // render the view
+    ctx.Redirect(ctx.URLFor("wiki.list"))
+  }
 }
