e68e842ca4b5c678d8d65f4201ea9ceaeb0f9068
Gregory Ballantine <brotherballantine@gmail.com>
2016-09-04 11:43:26 -0400

    Added Grunt to the project to generated CSS and JS files

---
+++ Gruntfile.js
@@ -0,0 +1,78 @@
+module.exports = function(grunt) {
+
+  // time the grunt execution time
+  require('time-grunt')(grunt);
+
+  // configure main project settings
+  grunt.initConfig({
+
+    // basic config
+    pkg: grunt.file.readJSON('package.json'),
+
+    // SASS compiling
+    sass: {
+      compile: {
+        options: {
+          sourcemap: 'none',
+          style: 'compact'
+        },
+        files: [{
+          expand: true,
+          cwd: 'assets/sass',
+          src: ['*.sass'],
+          dest: 'public/css',
+          ext: '.full.css'
+        }]
+      }
+    },
+
+    // CoffeeScript compiling
+    coffee: {
+      compile: {
+        expand: true,
+        flatten: true,
+        cwd: 'assets/coffee',
+        src: ['*.coffee'],
+        dest: 'public/js',
+        ext: '.full.js'
+      }
+    },
+
+    // Minify css files
+    cssmin: {
+      minify: {
+        files: [{
+          expand: true,
+          cwd: 'public/css',
+          src: ['*.full.css'],
+          dest: 'public/css',
+          ext: '.css',
+        }]
+      }
+    },
+
+    // Minify js files
+    uglify: {
+      minify: {
+        files: [{
+          expand: true,
+          cwd: 'public/js',
+          src: ['*.full.js'],
+          dest: 'public/js',
+          ext: '.js',
+        }]
+      }
+    }
+
+  });
+
+  // load plugins
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // do the tasks
+  grunt.registerTask('default', ['sass', 'coffee', 'cssmin', 'uglify']);
+
+};
